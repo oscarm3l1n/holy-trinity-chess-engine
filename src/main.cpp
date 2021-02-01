6caaf1e9376fed682e693343fb6224e34c096045
Oscar Melin <meos1713@student.ju.se>
2021-02-01 19:38:45 +0100

    lets stop fooling around and get down to bizniz

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,6 @@
 #include "bitboard.h"
 
 int main(){
-
-    #ifdef WIN64
-        std::cout << "Aeee..." << std::endl;
-    #else
-        std::cout << "Tjena kexet" << std::endl;
-    #endif
-
     u64 bitboard = 0ULL;
 
     printf("Bitboard: %lld\n", bitboard);
