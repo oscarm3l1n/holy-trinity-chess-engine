b945063552e64404e7ea700a6b65b50952f1b7b6
Oscar Melin <meos1713@student.ju.se>
2021-02-04 16:59:35 +0100

    created macros for the move. also created encode_move function that sets different bits in an integer

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "attack.cpp"
 #include "uci.cpp"
 #include "init.cpp"
+#include "moves.cpp"
 
 #define STARTFEN "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 
@@ -14,11 +15,13 @@ int main(){
     init();
     init_leaper_attacks();
 
-    parse_fen( (char*) "8/8/ppp5/8/8/3b4/8/8 w - - 0 1");
-    print_board();
+    // parse_fen( (char*) "8/8/ppp5/8/8/3b4/8/8 w - - 0 1");
+    // print_board();
 
-    print_attacked_squares(black);
-
+    // print_attacked_squares(black);
+    int move = encode_move(h1, h1, k, k, 1, 1, 1);
+    print_bitboard( (u64)move );
+    std::cout << move << std::endl;
 
     return 0;
 }
--- src/moves.cpp
+++ src/moves.cpp
@@ -1,14 +1,34 @@
-#include "bitboard.h"
+#include <iostream>
+#include <vector>
+// #include "bitboard.h"
 
-// int side is the attacking side
-bool square_attacked(int side, int square) {
+void add_move(std::vector<int>& moveList, int move) {
+    moveList.push_back(move);
+}
 
-    if (side == white) {
-        // Pawns
-        if ( pawnAttacks[black][square] & bitboards[P] ) return true;
-    } else {
+// store the whole move in an integer i.e. 32 bits
+// 00000000 00000000 00000000 00111111 fromSquare       6 bits
+// 00000000 00000000 00001111 11000000 toSquare         6 bits ( >> 6)
+// 00000000 00000000 11110000 00000000 piece            4 bits ( >> 12)
+// 00000000 00001111 00000000 00000000 promotedPiece    4 bits ( >> 16)
+// 00000000 00010000 00000000 00000000 captureFlag      1 bit  ( >> 20)
+// 00000000 00100000 00000000 00000000 doubleMoveFlag    1 bit  ( >> 21)
+// 00000000 01000000 00000000 00000000 enPassantFlag    1 bit  ( >> 22)
+// 00000000 10000000 00000000 00000000 castlingFlag     1 bit  ( >> 23)
 
-    }
+int encode_move(int fromSq, int toSq, int piece, int promotedPiece, int captureFlag, int doubleMoveFlag, int enPassantFlag, int castlingFlag) {
+    int move = 0;
+    move |= fromSq;
+    move |= (toSq << 6);
+    move |= (piece << 12);
+    move |= (promotedPiece << 16);
+    move |= (captureFlag << 20);
+    move |= (doubleMoveFlag << 21);
+    move |= (enPassantFlag << 22);
+    move |= (castlingFlag << 23);
+    return move;
+}
 
-    return false;
+void add_move(std::vector<int>& moveList, int move){
+    moveList.push_back(move);
 }
